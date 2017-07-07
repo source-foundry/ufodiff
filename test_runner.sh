926832e0ae6e5f0472b92a906c9b3859384ef452
Chris Simpkins <git.simpkins@gmail.com>
2017-07-06 22:14:50 -0400

    updated CI test settings, added command line help/version/argument validation tests

---
+++ test_runner.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# test_runner.sh : local testing script
+
+tox -e py27,py36
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,3 +3,88 @@
 
 import sys
 import pytest
+
+# ///////////////////////////////////////////////////////
+#
+# pytest capsys capture tests
+#    confirms capture of std output and std error streams
+#
+# ///////////////////////////////////////////////////////
+
+
+def test_pytest_capsys(capsys):
+    print("bogus text for a test")
+    sys.stderr.write("more text for a test")
+    out, err = capsys.readouterr()
+    assert out == "bogus text for a test\n"
+    assert out != "something else"
+    assert err == "more text for a test"
+    assert err != "something else"
+
+
+# ///////////////////////////////////////////////////////
+#
+# Standard output tests for help, usage, version
+#
+# ///////////////////////////////////////////////////////
+
+def test_ufodiff_commandline_shorthelp(capsys):
+    with pytest.raises(SystemExit):
+        from ufodiff.app import main
+        sys.argv = ['ufodiff', '-h']
+        main()
+
+    out, err = capsys.readouterr()
+    assert out.startswith("====================================================") is True
+
+
+def test_ufodiff_commandline_longhelp(capsys):
+    with pytest.raises(SystemExit):
+        from ufodiff.app import main
+        sys.argv = ['ufodiff', '--help']
+        main()
+
+    out, err = capsys.readouterr()
+    assert out.startswith("====================================================") is True
+
+
+def test_ufodiff_commandline_shortversion(capsys):
+    with pytest.raises(SystemExit):
+        from ufodiff.app import main
+        from ufodiff.app import settings
+        sys.argv = ['ufodiff', '-v']
+        main()
+
+    out, err = capsys.readouterr()
+    assert out.startswith(settings.VERSION)
+
+
+def test_ufodiff_commandline_longversion(capsys):
+    with pytest.raises(SystemExit):
+        from ufodiff.app import main
+        from ufodiff.app import settings
+        sys.argv = ['ufodiff', '--version']
+        main()
+
+    out, err = capsys.readouterr()
+    assert out.startswith(settings.VERSION)
+
+# TODO: add usage request testing
+
+
+# ///////////////////////////////////////////////////////
+#
+# Standard output tests for argument validations
+#
+# ///////////////////////////////////////////////////////
+
+
+def test_ufodiff_commandline_missingargs(capsys):
+    with pytest.raises(SystemExit):
+        from ufodiff.app import main
+        sys.argv = ['ufodiff']
+        main()
+
+    out, err = capsys.readouterr()
+    assert err.startswith("[ufodiff] ERROR: Please include the appropriate arguments with your command.")
+
