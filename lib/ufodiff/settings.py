1bab7b6a4d9550742b377ced6339c5f7c4676c48
Chris Simpkins <git.simpkins@gmail.com>
2017-07-11 16:22:40 -0400

    in-app help documentation updates

--- lib/ufodiff/settings.py
+++ lib/ufodiff/settings.py
@@ -33,20 +33,27 @@ ufodiff is a source file diff tool for typefaces developed with UFO source.
 
 Subcommands:
 
-- delta         --- text output to standard output stream
+- delta         --- UFO source file add/del/mod as plain text
    - all
-- deltajson     --- json output to standard output stream
+- deltajson     --- UFO source file add/del/mod as JSON
    - all
-- deltamd       --- Markdown output to standard output stream
+- deltamd       --- UFO source file add/del/mod as Markdown
    - all
+- diff          --- colored text diff of UFO spec files (only)
+- diffnc        --- uncolored text diff of UFO spec files (only)
   
 Examples:
   ufodiff delta all commits:3 <optional UFO filter>
   ufodiff deltajson all commits:3 <optional UFO filter>
   ufodiff deltamd all commits:3 <optional UFO filter>
+  ufodiff diff commits:3
+  ufodiff diff branch:development
+  ufodiff diff master..development
+  ufodiff diff HEAD~3
   
-Increase or decrease integer value after the `commit:` argument to modify evaluation to that number of previous commits in the git repository.
+Increase or decrease integer value after the `commits:` argument to modify evaluation to that number of previous commits in the git repository.
 
+Specify the branch for comparison with your current branch after the `branch:` argument in the diff subcommands.
 """
 
 # ------------------------------------------------------------------------------
